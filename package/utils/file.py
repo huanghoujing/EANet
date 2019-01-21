9c317cc55ed1dd9476c1f077da968ca878caf1d6
houjing.huang <houjing.huang@yz-gpu004.hogpu.cc>
2019-01-21 17:12:46 +0800

    improve walkdir; rm pdf

--- package/utils/file.py
+++ package/utils/file.py
@@ -89,17 +89,19 @@ def get_files_by_pattern(root, pattern='a/b/*.ext', strip_root=False):
     return ret
 
 
-def walkdir(folder, ext=None, sub_path=False, abs_path=False):
+def walkdir(folder, exts=None, sub_path=False, abs_path=False):
     """Walk through each files in a directory.
     Reference: https://github.com/tqdm/tqdm/wiki/How-to-make-a-great-Progress-Bar
     Args:
-        ext: file extension
+        exts: file extensions, e.g. '.jpg', or ['.jpg'] or ['.jpg', '.png']
         sub_path: whether to exclude `folder` in the resulting paths, remaining sub paths
         abs_path: whether to return absolute paths
     """
+    if isinstance(exts, str):
+        exts = [exts]
     for dirpath, dirs, files in os.walk(folder):
         for filename in files:
-            if (ext is None) or (os.path.splitext(filename)[1] == ext):
+            if (exts is None) or (os.path.splitext(filename)[1] in exts):
                 path = os.path.join(dirpath, filename)
                 if sub_path:
                     path = path[len(folder) + 1:]
